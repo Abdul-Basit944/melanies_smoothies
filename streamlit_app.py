d17328478b83dfd32e9e7b61c412aeba6713551b
Abdul Basit <159782544+Abdul-Basit944@users.noreply.github.com>
2024-08-12 16:00:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,9 @@ if ingredients_list:
             st.error(f"An error occurred: {e}")
 
 
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
 
