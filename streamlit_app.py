9a3690ebbb32af4d39356fffec875ae980cbd65a
Abdul Basit <159782544+Abdul-Basit944@users.noreply.github.com>
2024-08-12 16:11:09 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,11 @@ ingredients_list = st.multiselect(
 if ingredients_list:
     # Build the ingredients string
     ingredients_string = ' '.join(ingredients_list)
+    for fruit_chosen in ingredients_list:
+             ingredients_string += fruit_chosen +' '
+             st.subheader(fruit_chosen + 'Nutrition Information ')
+             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+             fv_df = st.dataframe(data=fruityvice_response.json() , use_container_width=True)
 
     # Create the SQL insert statement
     my_insert_stmt = f"""INSERT INTO smoothies.public.orders (ingredients, name_on_order)
@@ -44,8 +49,7 @@ if ingredients_list:
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json() , use_container_width=True)
+
 
 
 
